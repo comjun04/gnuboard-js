000cd31b3a74a893eefe565e7b4ce76ee0bff4e6
comjun04 <comjun04@gmail.com>
2020-02-05 18:21:55 +0900

    force trailing slash

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const ejs = require('ejs')
+const slash = require('express-slash')
 
 const indexRouter = require('./routes/index')
 const installRouter = require('./routes/install')
@@ -8,10 +9,13 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', './page')
+app.enable('strict routing')
 
-app.use('/src', express.static('./src'))
+app.use('/src/', express.static('./src'))
+app.use(express.urlencoded({extended: true, limit: '50mb'}))
 
 app.use('/', indexRouter)
-app.use('/install', installRouter)
+app.use(slash())
+app.use('/install/', installRouter)
 
 app.listen(4000, () => {console.log('server started')})
--- routes/install.js
+++ routes/install.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router({strict: true})
 
 const ejs = require('ejs')
 const path = require('path').resolve()
 const fs = require('fs')
 const os = require('os')
+const slash = require('express-slash')
 
 const config = require('../config')
 
@@ -12,7 +13,28 @@ router.get('/', (req, res) => {
   res.set('Content-Type', 'text/html; charset=utf-8')
   res.set('X-Robots-Tag', 'noindex')
 
-  res.render('install/index', {config, fs, os})
+  res.render('install/index', {config, fs, os, path})
+})
+
+router.use(slash())
+
+router.post('/install_config/', (req, res) => {
+  let datenow = new Date()
+
+  let day = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  let month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  
+  let gmnow = `${day[datenow.getUTCDay()]}, ${datenow.getUTCDate()} ${month[datenow.getUTCMonth()]} ${datenow.getUTCFullYear()} ${datenow.getUTCHours()}:${datenow.getUTCMinutes()}:${datenow.getUTCSeconds()}`
+
+  res.set('Expires', 0) // rfc2616 - Section 14.21
+  res.set('Last-Modified', gmnow)
+  res.set('Cache-Control','no-store, no-cache, must-revalidate') // HTTP/1.1
+  res.set('Cache-Control','pre-check=0, post-check=0, max-age=0') // HTTP/1.1
+  res.set('Pragma','no-cache') // HTTP/1.0
+  res.set('Content-Type','text/html; charset=utf-8')
+  res.set('X-Robots-Tag','noindex')
+
+  res.render('install/install_config', {POST: req.body, config, fs, os})
 })
 
 module.exports = router
